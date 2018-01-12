6256c32b6e20c428c784f2dfa69828b776fc7bae
Jim Reid <reidj32@gmail.com>
2018-01-12 13:10:19 -0600

    removing the strong type from the service response to make the test pass

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,6 +13,6 @@ export class ProductService {
   constructor(private _http: Http) {}
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((res: Response) => <Album>res.json());
+    return this._http.get(this._albumUrl).map(res => <Album>res.json());
   }
 }
